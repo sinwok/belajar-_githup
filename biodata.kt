0f99547f0f24618b42d1507c16f88f468adbf1d8
sinwok <145539433+sinwok@users.noreply.github.com>
2023-09-20 15:11:35 +0700

    Create biodata.kt

---
+++ biodata.kt
@@ -0,0 +1,11 @@
+fun main()
+{
+  var nama = "hana"
+  var kelas = "XI RPL 2"
+  var hobi = "naik kepuncak gunung tinggi sekali"
+
+
+  println("Nama: $nama")
+  println("Kelas: $kelas")
+  println("Hobi: $hobi")
+}
